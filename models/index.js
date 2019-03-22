64659d5b984890a9caa209f36873021024f79b30
Angeline <atan@thoughtworks.com>
2019-03-22 14:00:27 +0800

    Create Book and Author models and seed file

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+
+// Create the a pre configured sequelize instance
+const sequelize = new Sequelize("books-api", "postgres", "", {
+  dialect: "postgres"
+});
+
+//pass the models to the connection
+const models = {
+  Book : sequelize.import("./Book"),
+  Author : sequelize.import("./Author")
+}
+
+Object.keys(models).forEach(key => {
+  if("associate" in models[key]){
+    models[key].associate(models);
+  }
+});
+
+module.exports = {
+  sequelize,
+  ...models
+};
+
+//Link up all models
